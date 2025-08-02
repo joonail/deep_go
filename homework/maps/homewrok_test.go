df0ec6aa3e9fcbc95b99b9019f70436b991cfd88
Pavel Lopatin <7375538+joonail@users.noreply.github.com>
2025-08-02 21:35:57 +0300

    Homework 4 Maps

--- homework/maps/homewrok_test.go
+++ homework/maps/homewrok_test.go
@@ -9,32 +9,114 @@ import (
 
 // go test -v homework_test.go
 
+type node struct {
+	key, value  int
+	left, right *node
+}
+
 type OrderedMap struct {
-	// need to implement
+	root *node
+	size int
 }
 
 func NewOrderedMap() OrderedMap {
-	return OrderedMap{} // need to implement
+	return OrderedMap{}
 }
 
 func (m *OrderedMap) Insert(key, value int) {
-	// need to implement
+	m.root = insertNode(m.root, key, value, &m.size)
+}
+
+func insertNode(n *node, key, value int, size *int) *node {
+	if n == nil {
+		*size++
+		return &node{key: key, value: value}
+	}
+	if key < n.key {
+		n.left = insertNode(n.left, key, value, size)
+	} else if key > n.key {
+		n.right = insertNode(n.right, key, value, size)
+	} else {
+		n.value = value
+	}
+	return n
 }
 
 func (m *OrderedMap) Erase(key int) {
-	// need to implement
+	var deleted bool
+	m.root, deleted = eraseNode(m.root, key)
+	if deleted {
+		m.size--
+	}
+}
+
+func eraseNode(n *node, key int) (*node, bool) {
+	if n == nil {
+		return nil, false
+	}
+	if key < n.key {
+		var deleted bool
+		n.left, deleted = eraseNode(n.left, key)
+		return n, deleted
+	}
+	if key > n.key {
+		var deleted bool
+		n.right, deleted = eraseNode(n.right, key)
+		return n, deleted
+	}
+
+	// Node found
+	if n.left == nil {
+		return n.right, true
+	}
+	if n.right == nil {
+		return n.left, true
+	}
+
+	successor := minNode(n.right)
+	n.key, n.value = successor.key, successor.value
+	n.right, _ = eraseNode(n.right, successor.key)
+	return n, true
+}
+
+func minNode(n *node) *node {
+	for n.left != nil {
+		n = n.left
+	}
+	return n
 }
 
 func (m *OrderedMap) Contains(key int) bool {
-	return false // need to implement
+	return containsNode(m.root, key)
+}
+
+func containsNode(n *node, key int) bool {
+	if n == nil {
+		return false
+	}
+	if key < n.key {
+		return containsNode(n.left, key)
+	} else if key > n.key {
+		return containsNode(n.right, key)
+	}
+	return true
 }
 
 func (m *OrderedMap) Size() int {
-	return 0 // need to implement
+	return m.size
 }
 
 func (m *OrderedMap) ForEach(action func(int, int)) {
-	// need to implement
+	inOrderTraversal(m.root, action)
+}
+
+func inOrderTraversal(n *node, action func(int, int)) {
+	if n == nil {
+		return
+	}
+	inOrderTraversal(n.left, action)
+	action(n.key, n.value)
+	inOrderTraversal(n.right, action)
 }
 
 func TestCircularQueue(t *testing.T) {
